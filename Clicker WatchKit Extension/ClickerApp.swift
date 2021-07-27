0d4cb05bb58bfde082b0915daa91d0b425e9bd87
Michael Adams <michael.adams135@icloud.com>
2021-07-27 13:50:13 -0400

    Initial Commit

---
+++ Clicker WatchKit Extension/ClickerApp.swift
@@ -0,0 +1,19 @@
+//
+//  ClickerApp.swift
+//  Clicker WatchKit Extension
+//
+//  Created by Michael Adams on 7/27/21.
+//
+
+import SwiftUI
+
+@main
+struct ClickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
---
+++ Clicker WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Clicker WatchKit Extension
+//
+//  Created by Michael Adams on 7/27/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
